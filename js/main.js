4cc878abb11418516a334d035ab42c02e49651e0
antden99 <antoniodenigris@icloud.com>
2024-02-19 14:33:40 +0100

    add request
    request of input

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 console.log("tutto ok")
+
+// Chiedo all'utente di inserire i km che desidera percorrere
+const distance = prompt("Inserisci i km che desireri percorrere");
+console.log(distance);
+
+// Chiedo all'utente di inserire la sua età
+const age = prompt("Inserisci la tua età");
+console.log(age);
+
+// Calcolo il prezzo del biglietto, moltiplicando la distanza da percorrere per il prezzo a km
+const price = (distance * 0.21);
+console.log(price);
